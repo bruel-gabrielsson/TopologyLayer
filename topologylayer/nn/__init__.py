ff141f7d552a57f684fda98002c53582715362f9
Brad Nelson <bjnelson@stanford.edu>
2019-05-27 15:14:30 -0700

    make extensions default

--- topologylayer/nn/__init__.py
+++ topologylayer/nn/__init__.py
@@ -3,5 +3,7 @@ from .features import TopKBarcodeLengths, PartialSumBarcodeLengths
 from .levelset import LevelSetLayer as LevelSetLayer2Dold
 from .levelset import LevelSetLayer1D as LevelSetLayer1Dold
 from .levelset_cpp import LevelSetLayer1D, LevelSetLayer2D
-from .rips import RipsLayer
-from .alpha import AlphaLayer
+from .rips import RipsLayer as RipsLayerOld
+from .alpha import AlphaLayer as AlphaLayerOld
+from .rips_cpp import RipsLayer
+from .alpha_cpp import AlphaLayer
