83e6cd16fc7888c3f37f163a6f2015bf48690433
Brad Nelson <bjnelson@stanford.edu>
2019-06-05 18:56:29 -0700

    test both algs in unittests

--- tests/nn/alpha.py
+++ tests/nn/alpha.py
@@ -10,31 +10,32 @@ class AlphaTest(unittest.TestCase):
         from topologylayer.nn import AlphaLayer
 
         # superlevel set
-        layer = AlphaLayer(maxdim=1)
-        x = torch.tensor([[1, 1], [1,-1], [-1,-1], [-1,1]], dtype=torch.float).requires_grad_(True)
+        for alg in ['hom', 'cohom']:
+            layer = AlphaLayer(maxdim=1, alg=alg)
+            x = torch.tensor([[1, 1], [1,-1], [-1,-1], [-1,1]], dtype=torch.float).requires_grad_(True)
 
-        dgms, issub = layer(x)
-        self.assertEqual(
-            issub,
-            True,
-            "Expected sublevel set layer")
-        self.assertEqual(
-            torch.all(torch.eq(remove_infinite_bars(remove_zero_bars(dgms[0]), issub),
-                        torch.tensor([[0., 2.], [0., 2.], [0., 2.]]))),
-            True,
-            "unexpected 0-dim barcode")
-        self.assertEqual(
-            torch.all(torch.eq(remove_zero_bars(dgms[1]),
-                        torch.tensor([[2., 2.8284270763397217]]))),
-            True,
-            "unexpected 1-dim barcode")
+            dgms, issub = layer(x)
+            self.assertEqual(
+                issub,
+                True,
+                "Expected sublevel set layer. alg=" + alg)
+            self.assertEqual(
+                torch.all(torch.eq(remove_infinite_bars(remove_zero_bars(dgms[0]), issub),
+                            torch.tensor([[0., 2.], [0., 2.], [0., 2.]]))),
+                True,
+                "unexpected 0-dim barcode. alg=" + alg)
+            self.assertEqual(
+                torch.all(torch.eq(remove_zero_bars(dgms[1]),
+                            torch.tensor([[2., 2.8284270763397217]]))),
+                True,
+                "unexpected 1-dim barcode. alg=" + alg)
 
-        d0 = remove_infinite_bars(remove_zero_bars(dgms[0]), issub)
-        p = torch.sum(d0[:, 1] - d0[:, 0])
-        p.backward()
+            d0 = remove_infinite_bars(remove_zero_bars(dgms[0]), issub)
+            p = torch.sum(d0[:, 1] - d0[:, 0])
+            p.backward()
 
-        self.assertEqual(
-            torch.all(torch.eq(x.grad,
-                torch.tensor([[1,1],[1,-1],[-1,0],[-1,0]], dtype=torch.float))),
-            True,
-            "unexpected gradient")
+            self.assertEqual(
+                torch.all(torch.eq(x.grad,
+                    torch.tensor([[1,1],[1,-1],[-1,0],[-1,0]], dtype=torch.float))),
+                True,
+                "unexpected gradient. alg=" + alg)
